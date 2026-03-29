39e41a2392c828d665309dce6914eb69a331e0c1
Veristian <veristianjarvico2@gmail.com>
2026-03-29 18:31:04 +0800

    Player Movement Force

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float maxMoveSpeed = 5f;
+    [SerializeField] private float moveForce = 5f;
+    [SerializeField] private float waterFriction = 5f;
 
+    private Rigidbody rb;
 
-    private void Update()
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
     {
         HandlePlayerMovement(InputManager.movementInput);
     }
@@ -13,9 +21,19 @@ public class PlayerMovement : MonoBehaviour
     private void HandlePlayerMovement(Vector2 movementInput)
     {
         if (Mathf.Abs(movementInput.x) < 0.1f && Mathf.Abs(movementInput.y) < 0.1f)
+        {
+            // Apply water friction when no input is given
+            Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            Vector3 frictionForce = -horizontalVelocity * waterFriction;
+            rb.AddForce(frictionForce, ForceMode.Acceleration);
             return;
-        Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        }
+        
+        Vector3 desiredVelocity = new Vector3(movementInput.x, 0, movementInput.y) * maxMoveSpeed;
+        Vector3 velocityChange = desiredVelocity - rb.linearVelocity;
+        velocityChange.y = 0; // Don't change vertical velocity
+        rb.AddForce(velocityChange * moveForce, ForceMode.Acceleration);
+
     }
 
 }
