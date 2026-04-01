3ddf1306d50c0f21500ad1458a5cd11272032e19
Veristian <veristianjarvico2@gmail.com>
2026-04-01 17:36:42 +0800

    Vignette Levels

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -1,14 +1,22 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class CameraManager : Singleton<CameraManager>
 {
     private CinemachineCamera cinemachineCamera;
+    [SerializeField]
+    private GameObject vignetteLv1;
+    [SerializeField]
+    private GameObject vignetteLv2;
+    [SerializeField]
+    private GameObject vignetteLv3;
 
     protected override void Awake()
     {
         base.Awake();
         cinemachineCamera = GetComponent<CinemachineCamera>();
+        
     }
 
     private void Start()
@@ -25,5 +33,25 @@ public class CameraManager : Singleton<CameraManager>
             LookAtTarget = player.transform,
             CustomLookAtTarget = false
         };  
+        IncrementVignetteLevel();
+
     }
+
+    public void IncrementVignetteLevel()
+    {
+        if (vignetteLv1 != null && !vignetteLv1.activeSelf)
+        {
+            vignetteLv1.SetActive(true);
+        }
+        else if (vignetteLv2 != null && !vignetteLv2.activeSelf)
+        {
+            vignetteLv2.SetActive(true);
+        }
+        else if (vignetteLv3 != null && !vignetteLv3.activeSelf)
+        {
+            vignetteLv3.SetActive(true);
+        }
+    }
+
+    
 }
