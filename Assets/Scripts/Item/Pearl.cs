57c1f9d81e7497d2cfa2722ec295880f4aa2797d
Veristian <veristianjarvico2@gmail.com>
2026-04-02 22:14:53 +0800

    Pearl

---
+++ Assets/Scripts/Item/Pearl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Pearl : MonoBehaviour
+{
+    private void Start()
+    {
+        SphereCollider collider = GetComponent<SphereCollider>();
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Add code here to give the player the pearl or trigger any desired effect
+            CameraManager.Instance.IncrementVignetteLevel(); // Increment the vignette level in the CameraManager
+            Destroy(gameObject); // Destroy the pearl after collection
+        }
+    }
+}
